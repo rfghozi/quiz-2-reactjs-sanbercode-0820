0f0afdd2cb15490679844601a343c6e2f8b4e49e
rfghozi <romzifarhan29@gmail.com>
2020-10-11 22:01:27 +0700

    update quis yang terbaru

--- soal12/soal12.js
+++ soal12/soal12.js
@@ -1,40 +1,43 @@
 class BangunDatar{
     constructor(name){
-        this._nama = name;
-    }
-
-    get name(){
-        return this._nama
-    }
-
-    set name(a){
-        this._nama = a;
-    }
-
-    luas(){
+        this._name = name;
         this._luas = "";
+        this._keliling = "";
+    }
+
+    get luas(){
         return this._luas;
     }
 
-    keliling(){
-        this._keliling = ""
+    set luas(a){
+        this._luas = a;
+    }
+
+    get keliling(){
         return this._keliling;
+    }
+
+    set keliling(b){
+        this._keliling = b;
     }
 }
 
 class Lingkaran extends BangunDatar{
-    constructor(name){
-        super(name);
+    constructor(){
+        super(this._keliling, this._luas);
+        this._luasLingkaran = 3.14 * r * r
+        this._kelilingLingkaran = 2 * 3.14 * r
     }
     
-    luas(r){
-        return console.log(this._luas);
+    luaslin(r){
+        return console.log(this._luasLingkaran);
     }
 
-    keliling(r){
-        return console.log(this._keliling);
+    kelilinglin(r){
+        return console.log(this._kelilingLingkaran);
     }
 }
+
 
 var bulat = new Lingkaran();
 Lingkaran.luas(7);
